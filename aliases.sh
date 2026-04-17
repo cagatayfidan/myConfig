b2ed98fb7ecaca15f5107b959f8176cf7e716078
cagatayfidan <cagatayfidan54@gmail.com>
2026-04-17 22:54:08 +0300

    flags added

--- aliases.sh
+++ aliases.sh
@@ -15,7 +15,16 @@ safe_alias() {
 }
 
 # Define your aliases safely
+# Defaults without flags
 safe_alias gc  'git checkout'
+safe_alias ga  'git add'
 safe_alias gst 'git status'
 safe_alias gb  'git branch'
 safe_alias gp  'git push'
+
+# With flags
+safe_alias gcm 'git commit -m'
+safe_alias gco 'git checkout -b'
+safe_alias gaa 'git add -A'
+safe_alias gbd 'git branch -d'
+safe_alias gpo 'git push origin'
